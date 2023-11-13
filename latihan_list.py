78348902cac26605c6d41a98f14c2f882bb6a845
Ahmad syaerozi <144976767+5520123003@users.noreply.github.com>
2023-11-13 10:10:35 +0800

    Add files via upload
    Hari ini saya membuat list sederhana hanya dengan print-print ajaa :-()

---
+++ latihan_list.py
@@ -0,0 +1,33 @@
+print(66*'=')
+print(f"| {'Nim':^5} | {'Nama':^18} | {'Usia':^5} | {'Status':^10} | {'Tinggi_badan':^5} |")
+print(66*'=')
+
+nim = ["03","04","05","06","07","08","09","10","11","12","13"]
+mahasiswa = ["indi","anggun","kartika","eka","silpie","istiana","lydia","jumahir","yudistira","multazam","Doni"]
+usia = [20,19,25,17,20,30,18,16,22,23,26]
+Status = [True,True,False,True,False,True,False,True,True,False,True]
+tinggi_badan = [150,123,155,175,176,154,153,149,147,135,171]
+
+print(f"|{nim[0]:^6} | {mahasiswa[0]:^18} | {usia[0]:^5} | {Status[0]:^10} | {tinggi_badan[0]:^13}|")
+print(66*'-')
+print(f"|{nim[1]:^6} | {mahasiswa[1]:^18} | {usia[1]:^5} | {Status[1]:^10} | {tinggi_badan[1]:^13}|")
+print(66*'-')
+print(f"|{nim[2]:^6} | {mahasiswa[2]:^18} | {usia[2]:^5} | {Status[2]:^10} | {tinggi_badan[2]:^13}|")
+print(66*'-')
+print(f"|{nim[3]:^6} | {mahasiswa[3]:^18} | {usia[3]:^5} | {Status[3]:^10} | {tinggi_badan[3]:^13}|")
+print(66*'-')
+print(f"|{nim[4]:^6} | {mahasiswa[4]:^18} | {usia[4]:^5} | {Status[4]:^10} | {tinggi_badan[4]:^13}|")
+print(66*'-')
+print(f"|{nim[5]:^6} | {mahasiswa[5]:^18} | {usia[5]:^5} | {Status[5]:^10} | {tinggi_badan[5]:^13}|")
+print(66*'-')
+print(f"|{nim[6]:^6} | {mahasiswa[6]:^18} | {usia[6]:^5} | {Status[6]:^10} | {tinggi_badan[6]:^13}|")
+print(66*'-')
+print(f"|{nim[7]:^6} | {mahasiswa[7]:^18} | {usia[7]:^5} | {Status[7]:^10} | {tinggi_badan[7]:^13}|")
+print(66*'-')
+print(f"|{nim[8]:^6} | {mahasiswa[8]:^18} | {usia[8]:^5} | {Status[8]:^10} | {tinggi_badan[8]:^13}|")
+print(66*'-')
+print(f"|{nim[9]:^6} | {mahasiswa[9]:^18} | {usia[9]:^5} | {Status[9]:^10} | {tinggi_badan[9]:^13}|")
+print(66*'-')
+print(f"|{nim[10]:^6} | {mahasiswa[10]:^18} | {usia[10]:^5} | {Status[10]:^10} | {tinggi_badan[10]:^13}|")
+print(66*'-')
+
